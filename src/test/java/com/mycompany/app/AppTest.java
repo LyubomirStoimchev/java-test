de9c8cdcecd4e93e6ff5a25355487d3da8e98330
Lyubomir Stoimchev <lyubomirstoimchev@Lyubomirs-MacBook-Air.local>
2023-03-06 20:00:07 +0200

    add selenium

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,21 +1,68 @@
 package com.mycompany.app;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
+
+@RunWith(JUnit4.class)
 public class AppTest {
     public AppTest() {
     }
+    public static WebDriver driver;
+    @BeforeClass
+    public static void vef(){
+        WebDriverManager.chromedriver().setup();
+        
+        driver = new ChromeDriver();
+    }
 
+    
     @Test
-    public void testApp() {
+    public void testApp() throws Exception {
+        
+
+        driver.get("https://github.com/bonigarcia/webdrivermanager");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        WebElement el = driver.findElement(By.xpath("//*[@class='f4 my-3']"));
+        
+        System.out.println(el.getText());
+
+        assertEquals(el.getText(), "asdasdasd");
+        driver.close();
+
         assertTrue(true);
     }
 
-    @Test
-    public void testMore() {
-        assertTrue(true);
+    @AfterClass
+    public static void asd(){
+        driver.close();
     }
+
+    // @After
+    // private void After(){
+    //     if (driver != null){
+    //         driver.close();
+    //     }
+    // }
 }
